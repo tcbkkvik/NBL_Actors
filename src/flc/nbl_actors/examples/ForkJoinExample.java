5e32e43f3348f29e2a4b4dbf2a84f714e13fc8dd
tcbkkvik <torbekkvik@gmail.com>
2014-08-18 13:49:07 +0200

    ForkJoinExample

--- src/flc/nbl_actors/examples/ForkJoinExample.java
+++ src/flc/nbl_actors/examples/ForkJoinExample.java
@@ -19,9 +19,39 @@ import java.util.concurrent.ExecutionException;
  */
 public class ForkJoinExample {
 
-    static String[] splitLeftRight(String s) {
-        int pos = s.length() / 2;
-        return new String[]{s.substring(0, pos), s.substring(pos)};
+    static String[] strSplit(String str, int no) {
+        String[] arr = new String[no];
+        int pos = 0;
+        for (int ix = 0; ix < no; ix++) {
+            int step = (int) Math.ceil((str.length() - pos) / (no - ix));
+            arr[ix] = str.substring(pos, pos + step);
+            pos += step;
+        }
+        return arr;
+    }
+
+    static String strJoin(String[] arr) {
+        StringBuilder sb = new StringBuilder();
+        for (String s : arr)
+            sb.append(s);
+        return sb.toString();
+    }
+
+    @SuppressWarnings("UnnecessaryLocalVariable")
+    static IASync<String> splitMerge2(IGreenThrFactory tf, String original) {
+        if (original.length() < 6)
+            return new ASyncDirect<>(original);
+        final String[] arr1 = strSplit(original, 5); //handles # > 2
+        final String[] arr2 = new String[arr1.length];
+        final ForkJoin<String> fj = new ForkJoin<>();
+        for (int i = 0; i < arr1.length; i++) {
+            final int ix = i;
+            fj.callAsync(tf.newThread(),
+                    () -> splitMerge2(tf, arr1[ix]),
+                    s -> arr2[ix] = s
+            );
+        }
+        return fj.resultAsync(s -> fj.setValue(strJoin(arr2)));
     }
 
     // Non-blocking Fork/Join example:
@@ -32,7 +62,7 @@ public class ForkJoinExample {
         if (original.length() < 6) return new ASyncDirect<>(original);
         ForkJoin<String> fj = new ForkJoin<>("");
         int count = 0;
-        for (String str : splitLeftRight(original)) {
+        for (String str : strSplit(original, 2)) { //only handles binary split
             final boolean isLeft = count++ == 0;
             fj.callAsync(tf.newThread()
                     , () -> splitMerge(tf, str)
@@ -47,7 +77,9 @@ public class ForkJoinExample {
             throws InterruptedException {
         final CountDownLatch latch = new CountDownLatch(1);
         log("Original string:\n " + origString);
-        factory.newThread().execute(
+        IGreenThr thr = factory.newThread();
+        log("\nAlgorithm 1 ..");
+        thr.execute(
                 () -> splitMerge(factory, origString)
                         .result(res -> {
                             log("Resulting string:\n " + res);
@@ -56,6 +88,15 @@ public class ForkJoinExample {
                         })
         );
         latch.await();
+        log("\nAlgorithm 2 ..");
+        thr.execute(
+                () -> splitMerge2(factory, origString)
+                        .result(res -> {
+                            log("Resulting string:\n " + res);
+                            assert origString.equals(res);
+                            latch.countDown();
+                        })
+        );
     }
 
     static void log(Object o) {
@@ -64,7 +105,7 @@ public class ForkJoinExample {
 
     @SuppressWarnings({"UnusedDeclaration", "Convert2MethodRef"})
     public static void main(String[] args) throws InterruptedException, ExecutionException {
-        try (IGreenThrFactory factory = new GreenThrFactory_single(4)) {
+        try (IGreenThrFactory factory = new GreenThrFactory_single(4, false)) {
             splitMergeDemo(factory,
                     "This is a test-string. Lets see if it comes back the same..");
         }
