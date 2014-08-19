115e439f88d62a5d872aa62549cf832931e09c5d
tcbkkvik <torbekkvik@gmail.com>
2014-08-19 09:41:24 +0200

    SpliteratorMain

--- src/flc/nbl_actors/examples/SpliteratorMain.java
+++ src/flc/nbl_actors/examples/SpliteratorMain.java
@@ -9,64 +9,81 @@ package flc.nbl_actors.examples;
 
 import flc.nbl_actors.core.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Spliterator;
-import java.util.concurrent.ExecutionException;
+import java.util.*;
+import java.util.concurrent.*;
 
 /**
- * Example of {@link Spliterator} usage.
- * Also, shows how the simpler {@link ASyncValue} can be
- * used instead of {@link ForkJoin}, if the fork/join is only binary,
+ * Example; Parallel {@link Spliterator} processing using {@link IGreenThr}.
  * Date: 18.08.14
  *
  * @author Tor C Bekkvik
  */
 public class SpliteratorMain {
-    /**
-     * Parallel sum
-     *
-     * @param factory thread factory
-     * @param stream  values
-     * @return resulting sum of values
-     */
-    static IASync<Float> splitSum(IGreenThrFactory factory, Spliterator<Float> stream) {
-        final ASyncValue<Float> ret = new ASyncValue<>(0f);
-        final Spliterator<Float> splitStream =
-                stream.estimateSize() > 10 ? stream.trySplit() : null;
-        if (splitStream != null) {
-            factory.newThread()
-                    .call(() -> splitSum(factory, splitStream)) //in parallel
-                    .result(partSum -> {
-                        ret.update(sum -> sum + partSum); //join sum
-                        ret.accept();
-                    });
-        } else ret.accept();
-        stream.forEachRemaining(val -> ret.update(sum -> sum + val));
-        return ret;//complete after ret.accept() called
+    static String str3(float val) {
+        return String.format(Locale.UK, " %.3f", val);
     }
 
     static List<Float> randomValues(int length) {
-        List<Float> list = new ArrayList<>();
         float sum = 0;
+        StringBuilder sb = new StringBuilder();
+        List<Float> list = new ArrayList<>();
         Random random = new Random();
         for (int i = 0; i < length; i++) {
             float rnd = random.nextFloat();
-            sum += rnd;
             list.add(rnd);
+            sum += rnd;
+            sb.append(str3(rnd));
         }
-        System.out.println("tstSplitSum      sum = " + sum);
+        sb.insert(0, " sum: " + sum + "   ");
+        System.out.println(sb);
         return list;
     }
 
-    @SuppressWarnings({"UnusedDeclaration", "Convert2MethodRef"})
+    public interface SpConsumer<T> {
+        int splitSize();
+
+        void consume(Spliterator<T> data);
+    }
+
+    static <T> void parallel(IGreenThrFactory factory, Spliterator<T> data, SpConsumer<T> consumer) {
+        Spliterator<T> data2 = data.estimateSize() > consumer.splitSize() ? data.trySplit() : null;
+        if (data2 != null) {
+            factory.newThread().execute(() -> parallel(factory, data, consumer));
+            parallel(factory, data2, consumer);
+        } else
+            consumer.consume(data);
+    }
+
     public static void main(String[] args) throws InterruptedException, ExecutionException {
+        class Sum implements SpConsumer<Float> {
+            float value;
+
+            float getValue() {
+                return value;
+            }
+
+            @Override
+            public int splitSize() {
+                return 5;
+            }
+
+            @Override
+            public void consume(Spliterator<Float> data) {
+                float[] val = {0};
+                data.forEachRemaining(v -> val[0] += v);
+                synchronized (this) {
+                    value += val[0];
+                }
+            }
+        }
         try (IGreenThrFactory factory = new GreenThrFactory_single(4, false)) {
-            factory.newThread().execute(() ->
-                    splitSum(factory, randomValues(37).spliterator())
-                            .result(s -> System.out.println("tstSplitSum splitSum = " + s))
-            );
+            final Sum sum = new Sum();
+            parallel(factory, randomValues(20).spliterator(), sum);
+            CompletableFuture<Float> result = new CompletableFuture<>();
+            factory.setEmptyListener(() -> {
+                result.complete(sum.getValue());
+            });
+            System.out.println(" sum: " + result.get());
         }
     }
 }
